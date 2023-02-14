9235f9a9fb8d19b6ccb44bdbfb0782b9346a60fa
Scott Hood <scottdothood@gmail.com>
2023-02-14 13:52:24 +0000

    Mephisto animation speed inversely proportional to emotion/health

--- scenes/Mephistopheles.gd
+++ scenes/Mephistopheles.gd
@@ -12,11 +12,17 @@ func spawn() -> void:
 
 
 func die() -> void:
+  _set_playback_speed(1.0)
   Animations.play("die")
 
 
 func change_face(index: int) -> void:
+  _set_playback_speed(1.0 - index*0.33)
   HeadSprite.frame = index
+
+
+func _set_playback_speed(value: float) -> void:
+  Animations.playback_speed = value
 
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
